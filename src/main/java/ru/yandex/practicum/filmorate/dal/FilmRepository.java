c172f291a20abfe6e969bbff38b31f2a08ed71dd
Alexey Vasin <vasin-a.i@yandex.ru>
2026-02-21 23:31:00 +0300

    style: убрал неиспользуемый import

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal;
 
-import java.util.ArrayList;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
