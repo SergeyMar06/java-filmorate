00e01014bbb16e1eaf93b0a54be5f6674ff2fff0
Ishxan <ishmsu2@inbox.ru>
2026-02-21 21:19:59 +0300

    Platinum

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.service.ReviewService;
 
 import java.util.Collection;
-import java.util.Map;
 
 @Slf4j
 @RestController
