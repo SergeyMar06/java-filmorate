67f88bf536ff13efa9c3e1a9dfa86c45e46e737b
Sergey Markiyanov <markiyanovs06@rambler.ru>
2026-03-03 22:18:13 +0300

    Revert "Platinum1985 реализация функциональности отзывы и лайки"
    This reverts commit 13c5902da9e0180c672cc24a39b2747be0fb9b7a.

--- src/main/java/ru/yandex/practicum/filmorate/dal/BaseRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/BaseRepository.java
@@ -30,8 +30,8 @@ public class BaseRepository<T> {
         return jdbc.query(query, mapper, params);
     }
 
-    protected boolean delete(String query, Object... params) { // чтобы разное количество параметров принимал
-        int rowsDeleted = jdbc.update(query, params);
+    protected boolean delete(String query, long id) {
+        int rowsDeleted = jdbc.update(query, id);
         return rowsDeleted > 0;
     }
 
